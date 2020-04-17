f1a94bc029abc49e2c6276c4d634aa851a9b4281
vitkolos <vit.kolos@gmail.com>
2020-04-17 23:15:11 +0200

    sticky box

--- pages/_lang/caste-dotazy.vue
+++ pages/_lang/caste-dotazy.vue
@@ -4,26 +4,30 @@
             <h1 class="heading__title">Časté dotazy</h1>
         </section>  <!-- /heading -->
 
-        <main class="main" role="main">
-            <section class="section" v-for="section in sections" :id="section.section_anchor">
-                <h2 :class="'section__title section__title--' + section.color">{{ $t('web.faq.sections.' + section.section_id) }}</h2>
-                <div class="section__content">
-                    <div class="faq">
-                        <div v-for="(question, q_index) in section.questions" :id="question.anchor"
-                        :class="[q_index == 0 ? 'faq__item--open' : '', 'section__item faq__item']">
-                            <h3 class="faq__q">{{ $t('web.faq.questions.' + question.id + '.question') }}</h3>
-                            <div class="faq__a">
-                                <template v-for="(item, index) in Object.keys($i18n.messages[$i18n.fallbackLocale].web.faq.questions[question.id].answer).length">
-                                   <div v-if="['<ul>', '<ol>', '<h4>'].some(v => $t('web.faq.questions.' + question.id + '.answer[' + index + ']').substring(0, 4).includes(v))"
-                                   v-html="$t('web.faq.questions.' + question.id + '.answer[' + index + ']')"></div>
-                                   <p v-else v-html="$t('web.faq.questions.' + question.id + '.answer[' + index + ']')"></p>
-                                </template>
+        <main class="main main--flex" role="main">
+            <div>
+                <section class="section" v-for="section in sections" :id="section.section_anchor">
+                    <h2 :class="'section__title section__title--' + section.color">{{ $t('web.faq.sections.' + section.section_id) }}</h2>
+                    <div class="section__content">
+                        <div class="faq">
+                            <div v-for="(question, q_index) in section.questions" :id="question.anchor"
+                            :class="[q_index == 0 ? 'faq__item--open' : '', 'section__item faq__item']">
+                                <h3 class="faq__q">{{ $t('web.faq.questions.' + question.id + '.question') }}</h3>
+                                <div class="faq__a">
+                                    <template v-for="(item, index) in Object.keys($i18n.messages[$i18n.fallbackLocale].web.faq.questions[question.id].answer).length">
+                                    <div v-if="['<ul>', '<ol>', '<h4>'].some(v => $t('web.faq.questions.' + question.id + '.answer[' + index + ']').substring(0, 4).includes(v))"
+                                    v-html="$t('web.faq.questions.' + question.id + '.answer[' + index + ']')"></div>
+                                    <p v-else v-html="$t('web.faq.questions.' + question.id + '.answer[' + index + ']')"></p>
+                                    </template>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            <box/>
+                </section>
+            </div>
+            <div>
+                <box/>
+            </div>
         </main>
     </div>
 </template>
